3fd005fb6860be9721135f17f6fd837e4ea600cb
Morall0 <mau_ml@comunidad.unam.mx>
2025-02-21 10:58:09 -0600

    Basic files

---
+++ Main.java
@@ -0,0 +1,10 @@
+import gattto.Gattto;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Gattto.start(); 
+    }
+
+}
+
---
+++ gattto/Gattto.java
@@ -0,0 +1,10 @@
+package gattto;
+
+public class Gattto {
+
+    public static void start() {
+        System.out.println("Gattto started!");
+    }
+
+}
+
